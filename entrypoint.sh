838e80411ca005151e64dc76eb932e3715053b4f
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-11-24 16:42:25 +0000

    Fixing parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
 echo "Updating ownership permissions on directory: $1"
+echo "  user: $2"
 
+# The current working directory in the container is the workspace we need to check and modify
 ls -la .
