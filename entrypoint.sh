4d7038156ef30069061a34590f3ef4686e290e68
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-11-30 18:33:45 +0000

    Adding User UID reference

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,15 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
+export USER_UID=$1
+
 echo "Updating ownership permissions on workspace directory ${2}"
-echo "  user: $1"
+echo "  user: $USER_UID"
 
 # The current working directory in the container is the workspace we need to check and modify
 ls -la .
 
 echo
-echo "Looking for files/directories in current working directory not owned by \"$1\""
+echo "Looking for files/directories in current working directory not owned by \"$USER_UID\""
 echo
-find . ! -user $2 -exec $SCRIPT_PATH/update_file_object.sh "{}" "$1" \;
+find . ! -user $USER_UID -exec $SCRIPT_PATH/update_file_object.sh "{}" "$USER_UID" \;
